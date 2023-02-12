93e56c2e82e45c63fbfb5e4cce0f84b907a6f1f5
kaushikd207 <kaushikd207@gmail.com>
2023-02-12 22:34:15 +0530

    api changed

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ window.addEventListener('DOMContentLoaded', async () => {
 const fetchResult = async () => {
   div.innerHTML = '<h4>Loading...</h4>'
   const result = await fetch(
-    'https://programming-quotes-api.herokuapp.com/quotes/random'
+    'https://api.quotable.io/random'
   )
   const data = await result.json()
 
-  div.innerHTML = `<p class='quote'>" ${data.en} "</p><br/>
-   <h3>${data.author} </h3>
+  div.innerHTML = `<p class='quote'>" ${data?.content} "</p><br/>
+   <h3>${data?.author} </h3>
   `
 }
